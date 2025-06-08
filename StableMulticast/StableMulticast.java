b5f9967a5701d044d0cd787e791183fb812bf4a6
flavio <flavioborinjunior@gmail.com>
2025-06-08 12:58:35 -0300

    funcao de deliver em stablemulticast

--- StableMulticast/StableMulticast.java
+++ StableMulticast/StableMulticast.java
@@ -1,22 +1,25 @@
 package StableMulticast;
 
+import java.lang.reflect.Member;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class StableMulticast {
     private final String multicastGroupIP = "10.0.0.1";
-    private final int multicastPort = 123;
+    private final int multicastPort = 4446;
 
-    private IStableMulticast client;
-    private String id;
+    private final IStableMulticast client;
+    private final String id;
 
-    private List<Integer> grupo = new ArrayList<Integer>();
-    private ArrayList<Message> messageBuffer = new ArrayList<Message>();
-    private MatrixClock matrix = new MatrixClock();
+    private final ArrayList<Message> messageBuffer = new ArrayList<Message>();
+    private final MatrixClock matrix = new MatrixClock();
+    private final Set<Member> group = ConcurrentHashMap.newKeySet();
     
 
     public StableMulticast(String multicastIP, Integer multicastPort, IStableMulticast client) {
@@ -56,5 +59,13 @@ public class StableMulticast {
 
     private void enviarPresencaPeriodicamente(String ip, int port) {
         // socket.send("DISCOVERY:<ip>:<porta>")
+    }
+
+    private void deliverMessages() {
+        for (Message m : messageBuffer) {
+            if(matrix.isReadyToDeliver(this.id, m)){
+                client.deliver(m.getContent());
+            }
+        }
     }
 }
